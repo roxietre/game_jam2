f1072af353f9e3b34055a1e7c802653055b956bd
Paul Atzenhoffer <paul.atzenhoffer@epitech.eu>
2022-03-26 19:17:59 +0100

    [ADD] aleatoire chambers

---
+++ GameJamBlueXoxo/Assets/Prefabs/ChambersTemplate.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChambersTemplate : MonoBehaviour
+{
+    // Start is called before the first frame update
+	public GameObject[] bottomChambers;
+	public GameObject[] topChambers;
+	public GameObject[] leftChambers;
+	public GameObject[] rightChambers;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
