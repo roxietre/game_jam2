40a6167a635262840e0bf2f8de153311600ec449
Tetris14 <tristan.gory@epitech.eu>
2022-03-26 21:30:56 +0100

    [ADD] Nerfed the egg and buffed the dmg

--- GameJamBlueXoxo/Assets/Prefabs/SpiderEggBehavior.cs
+++ GameJamBlueXoxo/Assets/Prefabs/SpiderEggBehavior.cs
@@ -5,19 +5,19 @@ using UnityEngine;
 public class SpiderEggBehavior : MonoBehaviour
 {
     public int health = 100;
-    public float spawnRate = 1f;
-    public float spawnTimer = 0f;
+    public float spawnRate = 2f;
+    private float spawnTimer = 0f;
     public GameObject normalSpider;
     public GameObject eSpider;
 
-    // a function that is called every 2 seconds and instantiates a normalSpider every seconds and a eSpider every 2 seconds
     void Update()
     {
         spawnTimer += Time.deltaTime;
         if (spawnTimer >= spawnRate)
         {
             spawnTimer = 0f;
-            if (Random.Range(0, 2) == 0)
+            int randomSpider = Random.Range(0, 2);
+            if (randomSpider == 0)
             {
                 Instantiate(normalSpider, transform.position, Quaternion.identity);
             }
@@ -26,7 +26,12 @@ public class SpiderEggBehavior : MonoBehaviour
                 Instantiate(eSpider, transform.position, Quaternion.identity);
             }
         }
-    }
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }   
 
     private void OnCollisionEnter2D(Collision2D other)
     {
