9cb40ece577fb9062c3aa8dba2a7a35c1192f0cb
Tetris14 <tristan.gory@epitech.eu>
2022-03-27 18:10:54 +0200

    [ADD] Updated eggspawn

--- GameJamBlueXoxo/Assets/egg_spawn.cs
+++ GameJamBlueXoxo/Assets/egg_spawn.cs
@@ -6,12 +6,19 @@ public class egg_spawn : MonoBehaviour
 {
     // create a randome number of object_egg to spawn in the map
 
+    public GameObject PowerUp;
     public GameObject object_egg;
     private int number_of_egg;
 
     public void Start()
     {
         number_of_egg = Random.Range(0, 3);
+
+        if (number_of_egg == 0)
+        {
+            Vector3 position = new Vector3(Random.Range(transform.position.x - 5 , transform.position.x +5 ), Random.Range(transform.position.y -5 , transform.position.y +5), 0);
+            Instantiate(object_egg, position, Quaternion.identity);
+        }
 
         for (int i = 0; i < number_of_egg; i++)
         {
